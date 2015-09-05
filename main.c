cd76379976baec4a7778e9178e2b43ed0a5773bf
liangkangkang <lkk15138194817@126.com>
2015-09-05 20:18:17 +0800

    add a main file

---
+++ main.c
@@ -0,0 +1,22 @@
+/*
+ *    File Name		: main.c
+ *    Author		: lkk
+ *    File decs		:
+ *    Mail			: 942504876@qq.com 
+ *    Created Time	: 2015年09月05日 星期六 20时08分07秒
+ */
+
+#include <stdio.h>
+
+
+/**!
+ *			main function 		
+ **/
+
+int
+main(int argc,char *argv[])
+{
+	printf("hello world");
+	return 0;
+}
+
